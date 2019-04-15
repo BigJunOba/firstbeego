cc5a591665959ed604febbf45bdc60e55de8555a
BigJunOba <sjy534948129@sina.com>
2019-04-15 15:08:12 +0800

    使用bee generate工具生成mvc中对应的文件

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	_ "firstbeego/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	orm.RegisterDataBase("default", "mysql", "root:bjtungirc@tcp(127.0.0.1:3306)/firstbeego")
 	beego.Run()
 }
-
